8ec4a95e502758c1d002021b1992dcd84566509b
Herbert Fasching <fasching.herbert@outlook.com>
2023-09-02 22:47:02 +0200

    items&blocks added

--- src/main/java/com/hiltihons/watermod/WaterMod.java
+++ src/main/java/com/hiltihons/watermod/WaterMod.java
@@ -14,6 +14,7 @@ import org.apache.logging.log4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(WaterMod.MOD_ID)
 public class WaterMod
+
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
